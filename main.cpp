30f6a28bbb5af37681cf447724dc2e5e3ede5dc3
koos <koos@kbuntu>
2018-10-03 14:20:37 -0700

    cleaned up a few things :)

--- main.cpp
+++ main.cpp
@@ -47,69 +47,66 @@ std::vector<std::string> read_names(std::string txtfilepath)
 
 int main(int argc, char** argv) 
 {
+    //Neural network settings
+    float net_confidence = 0.10f;
+    float net_threshold = 0.20f;
     std::string net_config = "/home/koos/ml/darknet/cfg/yolov3-spp.cfg";
     std::string net_weights = "/home/koos/ml/darknet/weights/yolov3-spp.weights";
     std::string net_names = "/home/koos/ml/darknet/data/coco.names";
-            
-    float net_confidence = 0.10f;
-    float net_threshold = 0.20f;
+ 
+    //Create Darknet Detector
+    Detector darknet(net_config, net_weights);
 
-    //Darknet Detector
-    Detector* darknet = new Detector(net_config, net_weights);
-
-    //load names.
+    //vector for class names.
      std::vector<std::string> names_list = read_names(net_names);
      
     //detection area bounding box color
     cv::Scalar obj_box_color = cv::Scalar(0x00, 0xff, 0x00);
 
-    //rc image
+    //opencv image
     cv::Mat src1;
     
-    cv::VideoCapture stream1(1); //0 is the id of video device.0 if you have only one camera.
+    //capture video stream from cam 0 at specific camera res.. 
+    cv::VideoCapture stream1(0); //0 is the id of video device.0 if you have only one camera.
     stream1.set(3,1280); // resolution X
     stream1.set(4,960); //resolution Y
     
-            
+    //make sure video stream is open/active
     if (!stream1.isOpened())
     { //check if video device has been initialised
         std::cout << "Error loading video" << std::endl;
         return -1;
     }    
     
- //unconditional loop
+    //unconditional loop while vide is feeding..
     while (true)
     {
         stream1.read(src1);
         if (!src1.data)
-            std::cout << "Image not loaded";
+        {
+            std::cout << "No image data" << std::endl;
+            break;
+        }
         else
         {
-            //detect objects!
-            std::vector<bbox_t> bboxes = darknet->detect(src1, 0.1, true);
+            //detect objects using yolo detector & return vector of bounding boxes!
+            std::vector<bbox_t> bboxes = darknet.detect(src1, net_threshold, true);
             
-            
-            if (bboxes.size() > 0)
+            //check objects location relative to areas of interrest..
+            for (auto obj_box : bboxes)
             {
-                //check objects location relative to areas of interrest..
-                for (auto obj_box : bboxes)
+                //only mark higher than 30% probability
+                if (obj_box.prob > 0.15)
                 {
-                    //only mark higher than 30% probability
-                    if (obj_box.prob > 0.15)
-                    {
-                        cv::Point obj_box_tl = cv::Point(obj_box.x, obj_box.y);
-                        cv::Point obj_box_br = cv::Point(obj_box.x + obj_box.w, obj_box.y + obj_box.h);
-                        cv::rectangle(src1, obj_box_tl, obj_box_br, obj_box_color, 2, 8, 0 );
-                        
-                        std::stringstream strstrm;
-                        strstrm << names_list.at(obj_box.obj_id) << " " << (int)(obj_box.prob * 100) << "%";
-                        cv::putText(src1, strstrm.str().c_str(), obj_box_tl, cv::FONT_HERSHEY_PLAIN, 2,  obj_box_color, 2);
-                    }
-                    
-                    //cout << obj_box.prob << endl;
-                 
+                    cv::Point obj_box_tl = cv::Point(obj_box.x, obj_box.y);
+                    cv::Point obj_box_br = cv::Point(obj_box.x + obj_box.w, obj_box.y + obj_box.h);
+                    cv::rectangle(src1, obj_box_tl, obj_box_br, obj_box_color, 2, 8, 0 );
+
+                    std::stringstream strstrm;
+                    strstrm << names_list.at(obj_box.obj_id) << " " << (int)(obj_box.prob * 100) << "%";
+                    cv::putText(src1, strstrm.str().c_str(), obj_box_tl, cv::FONT_HERSHEY_PLAIN, 2,  obj_box_color, 2);
                 }
-            
+
             }
             
             cv::imshow("cam", src1);
@@ -128,9 +125,6 @@ int main(int argc, char** argv)
             cv::imwrite("outfile.jpg", src1);
         }
     }
-    
-    //close yolo..
-    delete darknet;
     
     return 0;
 }
